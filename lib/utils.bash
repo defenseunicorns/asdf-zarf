4136a0e8ba0465380838f46e66b2eb1c47781a8a
Andy Roth <roth.andy@gmail.com>
2023-05-12 13:33:33 -0700

    wip

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for zarf.
 GH_REPO="https://github.com/defenseunicorns/zarf"
 TOOL_NAME="zarf"
 TOOL_TEST="zarf version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if zarf has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -71,7 +68,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert zarf executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
